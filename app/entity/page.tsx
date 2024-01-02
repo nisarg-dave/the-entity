95cf7fa057f50537b27553df0c820b930b3b754d
Nisarg Dave <ndave630@gmail.com>
2024-01-02 21:14:56 +0800

    created particles canvas

---
+++ app/entity/page.tsx
@@ -0,0 +1,12 @@
+import ParticlesCanvas from "@/components/ParticlesCanvas";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <ParticlesCanvas />
+    </div>
+  );
+}
+
+export default page;
